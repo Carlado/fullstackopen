c666c5b8bdaef36098ce79f0e969f6763ed25150
Mikael Adolfsson <mikael_adolfsson@live.se>
2022-08-28 12:57:09 +0200

    Phonebook step 1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName }]);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => {
+          return <li key={person.name}>{person.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
